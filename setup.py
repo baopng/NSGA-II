29a6ec33f87b32a7fb2596091e1a51c897106e7b
giabao <ngogiabaopham@gmail.com>
2018-05-14 02:06:55 +0700

    Update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-  name = 'nsga2',
+  name = 'nsga-2',
   version = '0.1',
   description = 'A NSGA-II implementation',
   long_description='A NSGA-II implementation',
